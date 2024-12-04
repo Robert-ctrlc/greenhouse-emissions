c45d7e2572a0b4ddb2a21f4191e94fcff2102797
Robert Manolache <c21711371@mytudublin.ie>
2024-12-04 16:56:58 +0000

    updates

---
+++ src/main/java/com/example/greenhouse_emissions/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.greenhouse_emissions.repository;
+
+import com.example.greenhouse_emissions.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
